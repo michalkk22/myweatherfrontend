439dffe1471a4c935d7ffcfe58e3176b0e4ba683
Michal Kolodziej <m.kolodziej2211@gmail.com>
2025-06-15 18:45:33 +0200

    integrated with browser geolocation api

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Footer from './components/footer/Footer';
+import { LocationType } from './types';
 
 const App = () => {
+  const [location, setLocation] = useState<LocationType | null>(null);
+
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      location => { setLocation({ latitude: location.coords.latitude, longitude: location.coords.longitude }) },
+      () => { setLocation({ latitude: 52.1, longitude: 21.0 }) });
+  }, []);
+
   return (
     <div>
       <Footer />
